1b04d6bbaeec8c526e46a10980c21d5e0aed5ea9
Suchitha Lochana <suchithald@gmail.com>
2019-03-02 01:50:44 +0530

    Updated routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'about', component: AboutComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
