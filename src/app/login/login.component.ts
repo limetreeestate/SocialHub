9e0f433ed81ea549002ee4a544c71d8f0dcf852a
Suchitha Lochana <suchithald@gmail.com>
2019-03-04 23:51:38 +0530

    Added login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../_services/database.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private database: DatabaseService) { }
 
   ngOnInit() {
+  }
+
+  login(event) {
+    event.preventDefault()
+    const target = event.target
+
+    //Get entered usename and password from the event
+    const email: String = target.querySelector("#email").value
+    const password: String = target.querySelector("#password").value
+
+    //Pass email and password for authentication to Database service
+    this.database.authenticateUser(email, password).subscribe(
+      data => {
+        console.log(data)
+      }
+    )
   }
 
 }
