08edbf0272ebd0a071b7dc484e5da9ad736ecab7
Suchitha Lochana <suchithald@gmail.com>
2019-04-02 00:00:27 +0530

    Implemented FaceBook search

--- src/app/_services/facebook-manager.service.ts
+++ src/app/_services/facebook-manager.service.ts
@@ -1,11 +1,82 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+
+declare var FB: any;
 
 @Injectable({
   providedIn: 'root'
 })
-export class FacebookManagerService {
 
-  constructor() { }
+
+export class FacebookManagerService  {
+
+
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    (window as any).fbAsyncInit = () => {
+      FB.init({
+        appId: '380519256063506',
+        status: true, 
+        cookie: true, 
+        xfbml: true,
+        version: 'v3.2'
+      })
+    }
+    
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'))
+  }
+
+  login(scope: object){
+    FB.login(
+      (res) => {
+      if (res.status == "connected") console.log(res)
+      else window.alert("error")
+      },
+      scope
+    )
+  }
+
+  logout() {
+    FB.logout((res) => {
+      console.log(res)
+    })
+  }
+
+  search(keywords: string, successCallback: any, errCallback): any {
+    //Get search keywords
+    let results = [];
+    
+    FB.api(
+      "me?fields=feed.limit(100){attachments{url},message}", //?fields=permalink_url
+      (response) => {
+        if (response && !response.error) {
+          console.log(response);
+          for (let post of response.feed.data) {
+            if (post.message == undefined) continue;
+            let message: string = post.message.toLowerCase();
+            if (message.includes(keywords)) {
+              results.push([post.message, post.attachments == undefined? null: post.attachments.data[0].url])
+            }
+          }
+          
+          console.log(results);
+          successCallback(results);
+          
+        } else {
+          errCallback(response.error)
+        }
+      }
+      );
+  }
+
 
   
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppManagerService } from '../_services/app-manager.service';
 import { Router } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FacebookManagerService } from '../_services/facebook-manager.service';
 
 
 @Component({
@@ -12,120 +13,52 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 export class SearchComponent implements OnInit {
 
   private results = [];
+  private keyword: string = "";
 
   constructor(
     private appManager: AppManagerService,
     private http: HttpClient,
-    private _router: Router
+    private _router: Router,
+    private _fb: FacebookManagerService
   ) { 
     
   }
 
   
-  /* loginFacebook(){
-    FB.login(
-      (response)=> {
-        console.log('submitLogin',response);
-        if (response.authResponse)
-        {
-          this.appManager.setUser('Suchitha', 'suchithald@gmail.com');
-          this.appManager.addAccount(response.authResponse["accessToken"]);
-        }
-        else
-        {
-          console.log('User login failed');
-        }
-      }, {
-        scope: "user_posts"
-      }
-    );
+  loginFacebook() {
+    const scope = "email, user_posts, user_photos, user_friends, user_videos"
+    this._fb.login({scope})
   }
 
-  search(event){
-    event.preventDefault();
-    const target = event.target;
-
-    //Get search keywords
-    const keywords: string = target.querySelector("#keywords").value.toLowerCase();
-    let results;
-    
-    FB.api(
-      "/me/feed?limit=100", //?fields=permalink_url
-      (response) => {
-        if (response && !response.error) {
-          console.log(response);
-          results = response.data;
-          for (let post of results) {
-            if (post.message == undefined) continue;
-            let message: string = post.message.toLowerCase();
-            if (message.includes(keywords)) {
-              this.results.push(post.message)
-            }
-          }
-          
-          console.log(this.results);
-          
-        } else {
-          
-        }
-      }
-      );
-      
-      
-    }
-    
-    logout(){
-      console.log(FB.getAuthResponse())
+  search() {
+    this._fb.search(
+      this.keyword,
+      res => this.results = res,
+      err => console.log(err)
+    )
+    console.log(this.results)
   }
+
+
+  ngOnInit(){
+
+    const fields = ["filterProfiles"]
+    this.http.post("http://localhost:8080/api/verify", {fields}).subscribe(
+      res => {
   
-  
-  ngOnInit() {
-    (window as any).fbAsyncInit = () => {
-      FB.init({
-        appId      : '380519256063506',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v3.2'
-      });
-      
-      FB.AppEvents.logPageView();   
-      
-    };
-    
-    (
-      function(doc, script, fbSDK){
-        var js, fjs = doc.getElementsByTagName(script)[0];
-        if (doc.getElementById(fbSDK)) {
-          return;
-        }
-        
-        js = doc.createElement(script); js.id = fbSDK;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-        
-      }(document, 'script', 'facebook-jssdk')
-      );
-    } */
-
-
-    ngOnInit(){
-
-      const fields = ["lName"]
-      this.http.post("http://localhost:8080/api/verify", {fields}).subscribe(
-        res => {
-    
-          if (res instanceof HttpErrorResponse) {
-            localStorage.removeItem("token")
-            window.alert("Invalid token")
-            this._router.navigate(["login"])
-          }
-          
-        },
-        err => {
-          console.log(err)
+        if (res instanceof HttpErrorResponse) {
           localStorage.removeItem("token")
+          window.alert("Invalid token")
           this._router.navigate(["login"])
         }
-      )
-    }
+        
+      },
+      err => {
+        console.log(err)
+        localStorage.removeItem("token")
+        this._router.navigate(["login"])
+      }
+    )
+    
+  }
 }
