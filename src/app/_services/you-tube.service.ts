42320636cd94cdaebfd2e29f41149380a8d2cae4
Suchitha Lochana <suchithald@gmail.com>
2019-05-13 12:58:46 +0530

    Implemented search filter with youtube API

--- src/app/_services/you-tube.service.ts
+++ src/app/_services/you-tube.service.ts
@@ -10,11 +10,10 @@ export class YouTubeService {
   private GoogleAuth: any // Google Auth object.
   constructor() {
     setTimeout(() => this.initClient(), 1000)
-    //this.GoogleAuth = gapi.auth2.getAuthInstance()
   }
-  
+
   private isAuthorized: boolean
-  
+
 
   //initialize google API to variable
   private initClient() {
@@ -29,34 +28,54 @@ export class YouTubeService {
 
     //Initialize Google API client object
     gapi.client.init(config).then(() => {
-        this.GoogleAuth = gapi.auth2.getAuthInstance()
+      this.GoogleAuth = gapi.auth2.getAuthInstance()
 
-        // Listen for sign-in state changes.
-        this.GoogleAuth.isSignedIn.listen(() => {
-          this.isAuthorized = true
-          console.log(this.isAuthorized)
-        })
+      // Listen for sign-in state changes.
+      this.GoogleAuth.isSignedIn.listen(() => {
+        this.isAuthorized = true
+        console.log(this.isAuthorized)
       })
+    })
+  }
+
+  ISODateString(d) {
+    console.log()
+    function pad(n) { return n < 10 ? '0' + n : n }
+    return d.getUTCFullYear() + '-'
+      + pad(d.getUTCMonth() + 1) + '-'
+      + pad(d.getUTCDate()) + 'T'
+      + pad(d.getUTCHours()) + ':'
+      + pad(d.getUTCMinutes()) + ':'
+      + pad(d.getUTCSeconds()) + 'Z'
+  }
+
+
+  search(keywords: string, filter, successCallback) {
+    //default parameters
+    let params = {
+      'part': 'snippet',
+      'q': keywords,
+      'type': 'video',
+      "maxResults": 20
     }
-    
-    search(keywords: string, callback) {
-      if (true) {
-        //Initialize API request
-        let request = gapi.client.youtube.search.list({
-          'part': 'snippet',
-          'q': keywords,
-          "maxResults": 20
-        });
-        console.log("ASDDD")
-        // Execute the API request.
-        request.execute((res) => {
-          callback(res)
-        });
-      }
-    }
-    
+    //Edit filter data params
+    delete filter.show
+    filter.publishedBefore = this.ISODateString(filter.publishedBefore)
+    filter.publishedAfter = this.ISODateString(filter.publishedAfter)
+    //Combine with filter parameters
+    params = { ...params, ...filter }
+    console.log(params)
+    //Initialize API request
+    let request = gapi.client.youtube.search.list(params);
+    console.log("ASDDD")
+    // Execute the API request.
+    request.execute(
+      (res) => successCallback(res)
+    );
+  }
+
   login() {
-      let res = this.GoogleAuth.signIn()
-      console.log(res)
+    let res = this.GoogleAuth.signIn()
+    console.log(res)
   }
 }
