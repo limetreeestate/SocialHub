82831ad541283ef503f009fe9847b8a2ac96c05c
Suchitha Lochana <suchithald@gmail.com>
2019-05-29 19:02:32 +0530

    Instead of callback returns observable

--- src/app/_services/twitter.service.ts
+++ src/app/_services/twitter.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { AuthService } from './authentication.service';
 import { DatePipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 declare var twttr: any;
 
@@ -18,7 +19,7 @@ export class TwitterService {
   }
 
   init() {
-    (window as any).twttr = (function(d, s, id) {
+    (window as any).twttr = (function (d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0],
         t = (window as any).twttr || {};
       if (d.getElementById(id)) return t;
@@ -26,31 +27,30 @@ export class TwitterService {
       js.id = id;
       js.src = "https://platform.twitter.com/widgets.js";
       fjs.parentNode.insertBefore(js, fjs);
-    
+
       t._e = [];
-      t.ready = function(f) {
+      t.ready = function (f) {
         t._e.push(f);
       };
-    
+
       return t;
     }(document, "script", "twitter-wjs"));
     console.log("Twitter widget initialized")
   }
 
-  search(keywords: string, filter, successCallback): void {
+  search(keywords: string, filter): Observable<any> {
     let filterNew = Object.assign({}, filter)
     delete filterNew.show
 
     filterNew.fromDate = new DatePipe("en-US").transform(filterNew.fromDate, "yyyyMMddhhmm") //Transform dates into formats required by the Search API
     filterNew.toDate = new DatePipe("en-US").transform(filterNew.toDate, "yyyyMMddhhmm") //Transform dates into formats required by the Search API
-    const params = {...{query: keywords}, ...filterNew}
+    const params = { ...{ query: keywords }, ...filterNew }
 
     const header = new HttpHeaders()
     header.append('Content-Type', 'application/X-www-form-urlencoded')
-    this.http.post("https://localhost:8080/api/twitter/search", params, {headers: header})
-      .subscribe(
-        (res) => successCallback(res),
-        (err) => console.log(err)
-      )
+    return this.http.post("/api/twitter/search", params, { headers: header })
   }
 }
+//2221757044785548
+//129417183848258
+//17841401395219715
